5e78b62b552a0eaa225de6e7d32fc5c9ba0db5a0
ateetyadav <ateet.yadav@gmail.com>
2017-06-18 01:02:13 +0100

    Update Splitter.sol

--- Splitter.sol
+++ Splitter.sol
@@ -9,44 +9,52 @@ contract Splitter {
 
         mapping (address => uint) balances;
 
-	event Transfer(address indexed _from, address indexed _to, uint256 _value);
-
+	//event Transfer(address indexed _from, address indexed _to, uint256 _value);
+	event LogTransfer(address indexed send, address indexed receiver, uint256 _value);
 
         function Splitter(address ad1, address ad2) {
 
-	bob=ad1;
-	carol=ad2;
-        //Initialize the balance with 10,000 Ether.	
-	balances[tx.origin] = 10000;
+		bob=ad1;
+		carol=ad2;
+       
         }
 
-         function xfer(address receiver1,address receiver2,uint amount) returns(bool sufficient) {
-		if (balances[msg.sender] < amount) return false;
-		amt=amount/2;
+        function xfer(address receiver1,address receiver2) returns(bool sufficient) {
+		if (balances[msg.sender] < msg.value) return false;
+		
+		Var rem=msg.value%2;   //check if remainder
+		var amt=msg.value/2;
 		balances[msg.sender] -= amount;
-		balances[receiver1] += amt;
-	 	Transfer(msg.sender, receiver1, amt);
-		balances[receiver2] += amt;
-		Transfer(msg.sender, receiver2, amt);
-		return true;
-	}
-        function getBalanceInEth(address addr) returns(uint){
-		return ConvertLib.convert(getBalance(addr),2);
-	}
+		if(rem==0){
+			balances[receiver1] += amt;
+			assert(balances[receiver1] + amt > balances[receiver1]);
+			return  balances[receiver1];
+	 		Transfer(msg.sender, receiver1, amt);
+			balances[receiver2] += amt;
+			assert(balances[receiver2] + amt > balances[receiver2]);
+			return  balances[receiver2];
+			Transfer(msg.sender, receiver2, amt);
+			return true;
+		} else{
+			balances[receiver1] += amt;
+			assert(balances[receiver1] + amt > balances[receiver1]);
+			return  balances[receiver1];
+	 		Transfer(msg.sender, receiver1, amt);
+			balances[receiver2] += amt;
+			assert(balances[receiver2] + amt > balances[receiver2]);
+			return  balances[receiver1];
+			Transfer(msg.sender, receiver2, amt);
+			if (balances[receiver2] + amt > balances[receiver2])
+			return  balances[receiver2];
 
+			balances[msg.sender]+=rem;  //add the remainder balance back to sender.
+			return true;
+		}
+
+	//}
+        
         function getBalance(address addr) returns(uint) {
 		return balances[addr];
 	}
 
-
- }      
-		
-
-
-
-
-
-
-	
-
-
+ } 
